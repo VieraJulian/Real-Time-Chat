99a162bc6630f86508b84fbb8d255b0e05a4bcb7
VieraJulian <julianviera2019@hotmail.com>
2025-01-05 14:49:40 -0300

    Finished chat app

--- src/main/java/com/example/chat/chat/ChatController.java
+++ src/main/java/com/example/chat/chat/ChatController.java
@@ -19,6 +19,8 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage,
                                 SimpMessageHeaderAccessor headerAccessor) {
+
+        // Add username in web socket session
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
     }
